4c505761f570b83913f9d429cf67b8027c2bf62d
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-12-24 09:25:59 -0500

    Update login.setup.ts

--- setup/login.setup.ts
+++ setup/login.setup.ts
@@ -4,10 +4,10 @@ test('login', async ({ page }) => {
   await page.goto('/');
 
   // username
-  await page.locator('[data-test="username"]').type('standard_user');
+  await page.locator('[data-test="username"]').type(process.env.user!);
   
   // password
-  await page.locator('[data-test="password"]').type('secret_sauce');
+  await page.locator('[data-test="password"]').type(process.env.MDP!);
   
   // login button
   await page.locator('[data-test="login-button"]').click();
